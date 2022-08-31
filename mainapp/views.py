d728decc9f17f6afbf5152fa9c7bc66de25a1cbb
Kostykovic Nikolay <nikolajkostykovic7@gmail.com>
2022-08-31 21:56:10 +0300

    Lesson 2

---
+++ mainapp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class ContactsView(TemplateView):
+    template_name = 'mainapp/contacts.html'
+
+class CoursesListView(TemplateView):
+    template_name = 'mainapp/courses_list.html'
+
+class DocSiteView(TemplateView):
+    template_name = 'mainapp/doc_site.html'
+
+
+class IndexView(TemplateView):
+    template_name = 'mainapp/index.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = "Привет вата"
+        return context_data
+
+class LoginView(TemplateView):
+    template_name = 'mainapp/login.html'
+
+
+class NewsView(TemplateView):
+    template_name = 'mainapp/news.html'
