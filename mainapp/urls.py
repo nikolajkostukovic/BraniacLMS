7e6bfecc6c17a9392f9215a29f201c95fe9fa9fd
Kostykovic Nikolay <nikolajkostykovic7@gmail.com>
2022-09-05 22:00:11 +0300

    "lesson_3 Urls in templates"

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,14 +1,15 @@
-from mainapp import views
 from django.urls import path
+
+from mainapp import views
 from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('contacts/', views.ContactsView.as_view()),
-    path('courses/', views.CoursesListView.as_view()),
-    path('docsite/', views.DocSiteView.as_view()),
-    path('', views.IndexView.as_view()),
-    path('login/', views.LoginView.as_view()),
-    path('news/', views.NewsView.as_view()),
+    path("", views.IndexView.as_view(), name="index"),
+    path("news/", views.NewsView.as_view(), name="news"),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path("contacts/", views.ContactsView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSiteView.as_view(), name="doc_site"),
+    path("login/", views.LoginView.as_view(), name="login"),
 ]
