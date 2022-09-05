936063fbbfc9ae3ebc5a3a9190aaea3367503d07
Kostykovic Nikolay <nikolajkostykovic7@gmail.com>
2022-09-05 16:44:13 +0300

    "lesson_3 Loops in templates"

--- mainapp/views.py
+++ mainapp/views.py
@@ -25,9 +25,15 @@ class LoginView(TemplateView):
 
 
 class NewsView(TemplateView):
-    template_name = 'mainapp/news.html'
+    template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
+        # Get all previous data
         context = super().get_context_data(**kwargs)
-        context['title'] = "Привет вата"
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
         return context
