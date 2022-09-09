caf528176c178536db18e91ed667cd8ca216ee43
Kostykovic Nikolay <nikolajkostykovic7@gmail.com>
2022-09-10 00:56:52 +0300

    "lesson_4 Display model's data"

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -8,8 +8,13 @@ app_name = MainappConfig.name
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("news/", views.NewsView.as_view(), name="news"),
-    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
+    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
     path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path(
+        "courses/<int:pk>/",
+        views.CoursesDetailView.as_view(),
+        name="courses_detail",
+    ),
     path("contacts/", views.ContactsView.as_view(), name="contacts"),
     path("doc_site/", views.DocSiteView.as_view(), name="doc_site"),
     path("login/", views.LoginView.as_view(), name="login"),
